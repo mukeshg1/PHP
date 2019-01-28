452ca7e5099a9e82d62d51b2b423a3c1fb7396a5
Mukesh Gupta <mukeshg@mindfiresolutions.com>
2019-01-28 19:29:47 +0530

    End of the day commit.

--- server.php
+++ server.php
@@ -7,32 +7,42 @@
 * @Author      : Mukesh
 * Comments     : The data entered through form is sent to MySQL database.
 */
-    //connect to the database
+   //connect to the database
     
 
-      include("constants.php");
-      $db = mysqli_connect(SERVER, USER, PASSWORD, DB);
+   $errors = 0;
+   include("constants.php");
+   $db = mysqli_connect(SERVER, USER, PASSWORD, DB);
 
-      session_start();
-      $errors = 0;
-      // receive all input values from the form
-      $firstname = mysqli_real_escape_string($db, $_REQUEST['firstname']);
-      $middlename = mysqli_real_escape_string($db, $_REQUEST['middlename']);
-      $lastame = mysqli_real_escape_string($db, $_REQUEST['lastname']);
-      $gender = mysqli_real_escape_string($db, $_REQUEST['gender']);
-      $birthdate = mysqli_real_escape_string($db, $_REQUEST['birthdate']);
-      $phone = mysqli_real_escape_string($db, $_REQUEST['phone']);
-      $email = mysqli_real_escape_string($db, $_REQUEST['email']);
-      $addr = mysqli_real_escape_string($db, $_REQUEST['addr']);
-      $password1 = mysqli_real_escape_string($db, $_REQUEST['password1']);
-      $password2 = mysqli_real_escape_string($db, $_REQUEST['password2']);
-      $about = mysqli_real_escape_string($db, $_REQUEST['about']);
+   
+   // receive all input values from the form
+   $firstname = mysqli_real_escape_string($db, $_POST['firstname']);
+   $middlename = mysqli_real_escape_string($db, $_POST['middlename']);
+   $lastame = mysqli_real_escape_string($db, $_POST['lastname']);
+   $gender = mysqli_real_escape_string($db, $_POST['gender']);
+   $birthdate = mysqli_real_escape_string($db, $_POST['birthdate']);
+   $phone = mysqli_real_escape_string($db, $_POST['phone']);
+   $email = mysqli_real_escape_string($db, $_POST['email']);
+   $addr = mysqli_real_escape_string($db, $_POST['addr']);
+   $password1 = mysqli_real_escape_string($db, $_POST['password1']);
+   $password2 = mysqli_real_escape_string($db, $_POST['password2']);
+   $about = mysqli_real_escape_string($db, $_POST['about']);
 
 
+   foreach($_POST as $key => $value) 
+	{
+		if (empty($value))
+		{
+			$errors = 1;
+		}
+   }
+   
+   if ($errors == 0)
+   {
       $password1 = password_hash($password1, PASSWORD_BCRYPT);
       $password2 = password_hash($password2, PASSWORD_BCRYPT);
       $sql = "INSERT INTO user (firstname, middlename, lastname, gender, birthdate, phone, email, addr, password1, password2, about) VALUES ('$firstname',
-            '$middlename', '$lastame', '$gender', '$birthdate', '$phone', '$email', '$addr', '$password1', '$password2', '$about')";
+         '$middlename', '$lastame', '$gender', '$birthdate', '$phone', '$email', '$addr', '$password1', '$password2', '$about')";
       if ($db->query($sql) === TRUE){
          echo "<p style='color:green;'> Registration successfull</p>";
       }
@@ -40,6 +50,7 @@
       {
          echo "<p style='color:red;'>Error:".mysqli_error($db)."</p>";
       }
+   }
     
   
 ?>  
